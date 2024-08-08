42d20169840db871108f18df3ae3001ee0a1547f
fortune-0 <fortunepeterspc07@gmail.com>
2024-08-08 01:06:45 +0100

    added new routes to define endpoints for connecting, disconnecting, and retrieving user information in the application

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 
 // Define routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
+router.get('/connect',  AuthController.getConnect);
+router.get('/disconnect',  AuthController.getDisconnect);
+router.get('/users/me', UserController.getMe);
+
 
 module.exports = router;
