2bf48d68e1fdb7baecdde81a9b156b6a1174748e
fortune-0 <fortunepeterspc07@gmail.com>
2024-08-10 05:57:49 +0100

    fixed typo

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const AppController = require('../controllers/AppController');
 const AuthController = require('../controllers/AuthController');
 const UsersController = require('../controllers/UsersController');
+const FilesController = require('../controllers/FilesController');
 
 
 // Define routes
@@ -11,7 +12,8 @@ router.get('/stats', AppController.getStats);
 router.post('/users', UsersController.postNew);
 router.get('/connect',  AuthController.getConnect);
 router.get('/disconnect',  AuthController.getDisconnect);
-router.get('/users/me', UserController.getMe);
+router.get('/users/me', UsersController.getMe);
+router.post('/files', FilesController.postUpload);
 
 
 module.exports = router;
